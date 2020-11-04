603aaff01e8374b9a8f0a8344752eff87414e606
Darren Lim <darren.limfh@ncs.com.sg>
2020-11-05 07:45:02 +0800

    Populate station markers from DB connection + Leaflet.MarkerCluster

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
 from flask import render_template
 from flask import Flask
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'database-atw2.cvfgbrtjh8kr.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'darren'
+app.config['MYSQL_PASSWORD'] = 'aroundtheworld'
+app.config['MYSQL_DB'] = 'noaahourly'
+
+mysql = MySQL(app)
+
+bla = "ho"
+ble = "hu"
 
 @app.route('/')
 def index(name=None):
-    return render_template('index.html', name=name)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM stations")
+    data = list(cur.fetchall())
+
+    # convert list of tuples into list of lists
+    data = [list(row) for row in data]
+
+    # some station names have backslashes causing parsing errors
+    for row in data:
+        if row[1] is not None:
+            row[1] = row[1].replace("\\", "")
+
+    cur.close()
+
+    return render_template('index.html', name=name, data=data)
