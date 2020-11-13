04067bb20997ea5d753c33af22788eac9d9d95ba
Darren Lim <darren.limfh@ncs.com.sg>
2020-11-14 00:03:20 +0800

    All displayed stations now have at least 1 similarity month, button months without similarity are now color-coded differently

--- app/main.py
+++ app/main.py
@@ -15,26 +15,47 @@ db_similarity = MySQLdb.connect(host="database-atw2.cvfgbrtjh8kr.us-east-1.rds.a
                                 db="similarity")
 
 stations_dict = {}
+similarity_tables_list = []
 
 
 @app.route('/')
 def index():
-    cur = db_noaahourly.cursor()
-    cur.execute("SELECT * FROM stations")
-    stations_list = list(cur.fetchall())
-    cur.close()
+    cur_noaahourly = db_noaahourly.cursor()
+    cur_similarity = db_similarity.cursor()
+    stations_list = []
 
-    # convert list of tuples into list of lists
-    stations_list = [list(row) for row in stations_list]
+    try:
+        # get list of all stations
+        cur_noaahourly.execute("SELECT * FROM stations")
+        stations_list = [list(row) for row in list(cur_noaahourly.fetchall())]
 
-    # some station names have backslashes causing parsing errors
-    for row in stations_list:
-        if row[1] is not None:
-            row[1] = row[1].replace("\\", "")
+        # some station names have backslashes causing parsing errors
+        for row in stations_list:
+            if row[1] is not None:
+                row[1] = row[1].replace("\\", "")
 
-    # convert list of lists into dictionary
-    for row in stations_list:
-        stations_dict[row[0]] = row
+        # convert list of lists into dictionary
+        for row in stations_list:
+            stations_dict[row[0]] = row
+
+        # get list of similarity tables
+        cur_similarity.execute("SHOW TABLES")
+        global similarity_tables_list
+        similarity_tables_list = [row[0] for row in list(cur_similarity.fetchall())]
+
+        # get list of stations with at least 1 similarity table
+        similarity_tables_list_unique = [int(table[table.find("_")+1:table.rfind("_")]) for table in
+                                         similarity_tables_list if table.count("_") == 2]
+        similarity_tables_list_unique = list(dict.fromkeys(similarity_tables_list_unique))
+
+        # filter list of all stations so that remaining stations each have at least 1 similarity table
+        stations_list = [stations_dict[table] for table in similarity_tables_list_unique
+                         if table in stations_dict.keys()]
+    except Exception as e:
+        print(e)
+    finally:
+        cur_noaahourly.close()
+        cur_similarity.close()
 
     return render_template('index.html', data=stations_list)
 
@@ -43,29 +64,34 @@ def index():
 def similarity():
     station = request.args.get("station")
     month = request.args.get("month")
-    limit = request.args.get("limit")
-    similar_stations_obj = {"stations": []}
+    limit = 100
     cur = db_similarity.cursor()
+    similar_stations_obj = {"stations": [], "available": []}
 
-    try:
-        cur.execute("SELECT * FROM station_" + station + "_" + month + " LIMIT 100")
-        similar_stations_list = list(cur.fetchall())
+    if station is not None and month is not None and limit is not None:
+        available = []
 
-        # convert list of tuples into list of lists
-        similar_stations_list = [list(row) for row in similar_stations_list]
+        for i in range(1, 13):
+            if "station_" + station + "_" + str(i) in similarity_tables_list:
+                available.append(i)
 
-        stations = []
+        similar_stations_obj["available"] = available
 
-        for row in similar_stations_list:
-            station = stations_dict[row[0]]
-            if station is not None:
-                station.append(row[1])
-                stations.append(station)
+        try:
+            cur.execute("SELECT * FROM station_" + station + "_" + month + " LIMIT " + str(limit))
+            similar_stations_list = [list(row) for row in list(cur.fetchall())]
+            stations = []
 
-        similar_stations_obj["stations"] = stations
-    except Exception as e:
-        print(e)
-    finally:
-        cur.close()
+            for row in similar_stations_list:
+                station = stations_dict[row[0]]
+                if station is not None:
+                    station.append(row[1])
+                    stations.append(station)
+
+            similar_stations_obj["stations"] = stations
+        except Exception:
+            pass
+        finally:
+            cur.close()
 
     return similar_stations_obj
