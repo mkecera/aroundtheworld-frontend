98ded48a4e9b0b46c13b7dfefade2380f65940a7
Michal Kecera <mkecera@gmail.com>
2020-11-18 08:07:59 +0100

    limit to elevation >-300

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def index():
 
     try:
         # get list of all stations
-        cur_noaahourly.execute("SELECT * FROM stations")
+        cur_noaahourly.execute("SELECT * FROM stations WHERE elevation>-300")
         stations_list = [list(row) for row in list(cur_noaahourly.fetchall())]
 
         # some station names have backslashes causing parsing errors
